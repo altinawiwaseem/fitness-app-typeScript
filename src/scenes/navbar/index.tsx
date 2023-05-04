d0492129d279a919d55630ac69f70a9a65dd063f
altinawiwaseem <wasemm@live.com>
2023-05-04 21:54:03 +0200

    create ActionButton

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -4,6 +4,7 @@ import Logo from "@/assets/logo.png";
 import Link from "./Link";
 import { SelectedPage } from "@/shared/types";
 import useMediaQuery from "@/hooks/useMediaQuery";
+import ActionButton from "@/shared/ActionButton";
 
 type Props = {
   selectedPage: SelectedPage;
@@ -13,7 +14,7 @@ type Props = {
 const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
   const flexBetween = "flex items-center justify-between";
   const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
-  const isAboveMediumScreens = useMediaQuery("min-width: 1060px");
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
   return (
     <nav>
       <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
@@ -49,7 +50,9 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
                 </div>
                 <div className={`${flexBetween} gap-8`}>
                   <p>Sign In</p>
-                  <button>Become a Member</button>
+                  <ActionButton setSelectedPage={setSelectedPage}>
+                    Become a Member
+                  </ActionButton>
                 </div>
               </div>
             ) : (
@@ -57,7 +60,7 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
                 className="rounded-full bg-secondary-500 p-2 "
                 onClick={() => setIsMenuToggled(!isMenuToggled)}
               >
-                <Bars3Icon />
+                <Bars3Icon className="h-6 w-6 text-white" />
               </button>
             )}
           </div>
